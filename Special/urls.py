092dc6eb6bc9cd595bc06c3368219aec967b4ba9
Arsh Goel <arshgoel16@gmail.com>
2024-11-23 10:56:40 +0530

    first commit

---
+++ Special/urls.py
@@ -0,0 +1,12 @@
+from django.views.generic import TemplateView
+from django.contrib import admin
+from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("auth/", include('Accounts.urls')),
+    path("",TemplateView.as_view(template_name = "home.html")),
+    path("dashboard", include('Services.urls')),
+]
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
