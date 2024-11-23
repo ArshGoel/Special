1b7a7cc4172b90481d64fe70dd543b8b2851b0b2
Arsh Goel <arshgoel16@gmail.com>
2024-11-23 11:09:25 +0530

    new

--- Special/settings.py
+++ Special/settings.py
@@ -19,7 +19,7 @@ ALLOWED_HOSTS = [".vercel.app","*"]
 # Application definition
 
 INSTALLED_APPS = [
-    # 'whitenoise.runserver_nostatic',
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
