b84923f9d1be19391c4352137b878b142ee68a76
Arsh Goel <arshgoel16@gmail.com>
2024-11-23 22:10:12 +0530

    final

--- Services/urls.py
+++ Services/urls.py
@@ -3,4 +3,5 @@ from Services import views
 
 urlpatterns = [
     path('',views.dashboard,name="dashboard"),
+    path('/final',views.final,name="final"),
 ]
--- Services/views.py
+++ Services/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def dashboard(request):
     return render(request,'dashboard.html')
+
+def final(request):
+    return render(request,'final.html')
